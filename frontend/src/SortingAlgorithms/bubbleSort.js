b3eababca7bf33e104e796456af52cac5fc051ea
mckenzie3131 <mckenzie3131@gmail.com>
2021-06-22 18:17:59 -0600

    Add bubble sort animations

--- frontend/src/SortingAlgorithms/bubbleSort.js
+++ frontend/src/SortingAlgorithms/bubbleSort.js
@@ -7,17 +7,29 @@ export function getBubbleSortLogic(array){
 
 function bubbleSortHelper(array,animations) {
     let sorted = false
+    let round = 0
     // console.log(array)
     while(!sorted){
         sorted = true
-        for( let i = 1; i < array.length; i++){
+        for( let i = 1; i < array.length-round; i++){
+            let valueOne = array[i]
+            let valueTwo = array[i-1]
+            animations.push([i-1,i])
+            animations.push([i-1,i])
             if (array[i-1] > array[i]){
+                animations.push([i,valueTwo])
+                animations.push([i-1,valueOne])
                 let temp = array[i];
                 array[i] = array[i-1]
                 array[i-1] = temp
                 sorted = false;
+            }else{
+                animations.push([i-1,valueTwo])
+                animations.push([i,valueOne])
             }
+
         }
+        round++;
     }
 }
 
